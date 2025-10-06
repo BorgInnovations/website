3fefefbd6eef163a33579aa88012774ff77169a9
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2025-10-05 22:40:21 -0700

    neyooom

---
+++ menu.js
@@ -0,0 +1,12 @@
+function topMenu_load(){
+
+    document.getElementById("topMenu").innerHTML = '<ul><li><a href="./index.html">B0RG</a></li><li><a href="#news">Products</a></li><li><a class="nbus" onclick="showcontact()">Contact</a></li><li><a href="#about">About</a></li></ul>'
+  
+  }
+  
+  function footMenu_load(){
+  
+    document.getElementById("footMenu").innerHTML = '<p style="text-align: center;"><a href="./index.html"><img src="./img/icons/BI_1.svg" style="width: 200px;padding: 15px;" alt=""></a></p><p style="text-align: center;">© 2025 Jacob Borg</p>'
+  
+  }
+  
