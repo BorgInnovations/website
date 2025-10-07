af1f49856e29f819de84889980d030a4e9c061e7
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2025-10-07 00:00:15 -0700

    updated

--- menu.js
+++ menu.js
@@ -1,7 +1,10 @@
+
 function topMenu_load(){
 
-    document.getElementById("topMenu").innerHTML = '<ul><li><a href="./index.html">B0RG</a></li><li><a class="nbus" onclick="showproducts()">Products</a></li><li><a class="nbus" onclick="showcontact()">Contact</a></li><li><a href="#about">About</a></li></ul>'
+
   
+    document.getElementById("topMenu").innerHTML = '<ul><li><a href="./index.html">B0RG</a></li><li><a class="nbus" onclick="showproducts()">Products</a></li><li><a class="nbus" onclick="showcontact()">Contact</a></li></ul>'
+    document.getElementById('products').innerHTML = '<div class="card"><div class="card-heading"><h1>Products.</h1> </div><div class="card-body"><div><h3>C0RT3X 01</h3><p>A human-centric research platform.</p><p><a href="./c0rt3x.html"><button class="bbus">Start Using</button></a></p></div></div></div>'
   }
   
   function footMenu_load(){
