7686e34b7cbe364a3fc90242eae50b3566f676b0
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2025-10-15 15:18:23 -0700

    Alpha 1.2
    First IRL AI source rejected from indexing!

--- source/search.js
+++ source/search.js
@@ -18,13 +18,14 @@ const documents = [
     { id: , content: '',author: '', ai_score: '0%', url: ''},
 
   */
-  { id: 1, content: "Two Lectures on Comets",author: 'Winthrop, John', ai_score: '0%', url: 'https://www.jstor.org/stable/community.32503417?seq=1'},
-  { id: 2, content: "A Biblical Chronology of the World, from Creation to Christ",author: 'A. W. Blakesley', ai_score: '0%', url: 'https://www.jstor.org/stable/community.39731890'},
-  { id: 3, content: 'A Defence of the Protestant Bible',author: 'Akroatees', ai_score: '0%', url: 'https://www.jstor.org/stable/community.39731889'},
-  { id: 4, content: 'A Discourse of the True Meaning of the Bible',author: 'Lady Cook', ai_score: '0%', url: 'https://www.jstor.org/stable/community.39731902'},
-  { id: 5, content: 'Library of the American Bible Union',author: 'T.J. Conant', ai_score: '0%', url: 'https://www.jstor.org/stable/community.39731906'},
-
-
+  { id: 1, content: "Two Lectures on Comets",author: 'Winthrop, John', ai_score: 'None', url: 'https://www.jstor.org/stable/community.32503417?seq=1'},
+  { id: 2, content: "A Biblical Chronology of the World, from Creation to Christ",author: 'A. W. Blakesley', ai_score: 'none', url: 'https://www.jstor.org/stable/community.39731890'},
+  { id: 3, content: 'A Defence of the Protestant Bible',author: 'Akroatees', ai_score: 'None', url: 'https://www.jstor.org/stable/community.39731889'},
+  { id: 4, content: 'A Discourse of the True Meaning of the Bible',author: 'Lady Cook', ai_score: 'None', url: 'https://www.jstor.org/stable/community.39731902'},
+  { id: 5, content: 'Library of the American Bible Union',author: 'T.J. Conant', ai_score: 'None', url: 'https://www.jstor.org/stable/community.39731906'},
+  { id: 6, content: 'Google users are less likely to click on links when an AI summary appears in the results',author: 'Athena Chapekis and Anna Lieb', ai_score: 'Low', url: 'https://www.pewresearch.org/short-reads/2025/07/22/google-users-are-less-likely-to-click-on-links-when-an-ai-summary-appears-in-the-results/'},
+  { id: 7, content: 'Vivaldi takes a stand: keep browsing human',author: 'Jon von Tetzchner', ai_score: 'Low', url: 'https://vivaldi.com/blog/keep-exploring/'},
+  { id: 8, content: 'Doing more, but learning less: The risks of AI in research',author: 'Mike Cummings', ai_score: 'Low', url: 'https://news.yale.edu/2024/03/07/doing-more-learning-less-risks-ai-research'},
 ];
 const pre = 'https://'
 function performSearch() {
@@ -42,7 +43,7 @@ function displayResults(results) {
   resultsElement.innerHTML = '';
   results.forEach(result => {
       const listItem = document.createElement('a');
-      listItem.innerHTML = '<span class="si">'+ result.content + '</span> <p>AI Score: '+result.ai_score+' | Author: '+result.author+'</p>';
+      listItem.innerHTML = '<span class="si">'+ result.content + '</span> <p>AI Likelihood: '+result.ai_score+' | Author: '+result.author+'</p>';
       listItem.style = 'color: var(--mercury);border-bottom: 0px solid var(--mercury);'
       listItem.target = '_blank'
       listItem.href = result.url
