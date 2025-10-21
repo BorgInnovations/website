64e21f36e43a59ee44c86b298bb4b1778f54f0b0
JB <48691704+JacobBorgProgramming@users.noreply.github.com>
2025-10-20 22:40:16 -0700

    updates to homepage

--- menu.js
+++ menu.js
@@ -3,8 +3,8 @@ function topMenu_load() {
 
 
 
-  document.getElementById("topMenu").innerHTML = '<ul><li><a href="./index.html">B0RG</a></li><li><a class="nbus" onclick="showproducts()">Products</a></li><li><a class="nbus" onclick="showcontact()">Contact</a></li></ul>'
-  document.getElementById('products').innerHTML = '<div class="card"><div class="card-heading"><h1>Products.</h1> </div><div class="card-body"><div><h3>C0RT3X 01</h3><p>A human-centric research platform.</p><p><a href="./c0rt3x.html"><button class="abus">Learn More</button></a></p><h3>Project Luminary</h3><p>Revisitng modern code standards.</p><p><a href="./luminary.html"><button class="abus">Learn More</button></a></p><h3>Author</h3><p>A browser based document editor.</p><p><a href="./author.html"><button class="abus">Learn More</button></a></p></div></div></div>'
+  document.getElementById("topMenu").innerHTML = '<ul><li><a href="./index.html">B0RG</a></li><li><a class="nbus" onclick="showproducts()">Projects</a></li><li><a class="nbus" onclick="showcontact()">Contact</a></li></ul>'
+  document.getElementById('products').innerHTML = '<div class="card"><div class="card-heading"><h1>Projects.</h1> </div><div class="card-body"><div><h3>C0RT3X 01</h3><p>A human-centric research platform.</p><p><a href="./c0rt3x.html"><button class="abus">Learn More</button></a></p><h3>Project Luminary</h3><p>Revisitng modern code standards.</p><p><a href="./luminary.html"><button class="abus">Learn More</button></a></p><h3>Author</h3><p>A browser based document editor.</p><p><a href="./author.html"><button class="abus">Learn More</button></a></p></div></div></div>'
 }
 
 function footMenu_load() {
