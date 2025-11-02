af1373e7bfb85355216de8275ee881c795cf8c4d
JB <48691704+JacobBorgProgramming@users.noreply.github.com>
2025-11-01 22:03:48 -0700

    began building quiz platform

---
+++ quizsource/amiaprot.js
@@ -0,0 +1,20 @@
+/* Am I a Prot or a Heretic Quiz Script */
+
+if (window.location.href == 'https://borginnovations.com/quizzes.html=utm_term=amiaprot') {
+    document.getElementById("quizzes").style.display = 'none'
+    document.getElementById("qheader").innerHTML = 'Am I a Prot or am I a Heretic?'
+    document.getElementById("quiz").style.display = 'block'
+
+}
+
+function amiaprot() {
+    document.getElementById("quizzes").style.display = 'none'
+    document.getElementById("qheader").innerHTML = 'Am I a Prot or am I a Heretic?'
+    document.getElementById("quiz").style.display = 'block'
+
+
+    /*
+        document.getElementById("quiz").innerHTML = 'cheese'
+        */
+}
+
