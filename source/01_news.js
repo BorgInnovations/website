378d491f2b51f8aeb8126baf022e043e59387f43
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2026-03-08 14:42:06 -0700

    Bug fixes

--- source/01_news.js
+++ source/01_news.js
@@ -1,5 +1,5 @@
 function loadNews(){
     var heading = '<h3>LuminaryIDE Alpha Testing</h3>'
-    var body = '<div> <p>LuminaryIDE, an integral part of the Luminary Project, has entered Alpha testing!</p> <p>The Luminary Project has the purpose of rediscovering the digital philosophies of our technologist forefathers:</p> <p>Innovation, Effeciency, Exploration.</p> <p> <a class="bbus" href="./01_luminary.html">Try out the LuminaryIDE</a> </p> </div>'
+    var body = '<div> <p>LuminaryIDE, an integral part of the Luminary Project, has entered Alpha testing!</p> <p>The Luminary Project has the purpose of rediscovering the digital philosophies of our technologist forefathers:</p> <p>Innovation, Efficiency, Exploration.</p> <p> <a class="bbus" href="./01_luminary.html">Try out the LuminaryIDE</a> </p> </div>'
     document.getElementById("newspanel").innerHTML = heading + body
 }
