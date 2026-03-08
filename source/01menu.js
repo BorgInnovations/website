d6f944c4903b729f39b5dc084d660718daed0ebe
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2026-03-07 20:02:57 -0800

    alpha build 3

--- source/01menu.js
+++ source/01menu.js
@@ -1,11 +1,10 @@
 function oiMenu_load() {
-    document.getElementById("menuList").innerHTML = '<div class="card-body"><center><p> <a href="./01.html" class="subMenuItem">Home</a> </p><p> <a href="./01_author" class="subMenuItem">Author|</a> </p><p> <a href="./01_c0rt3x.html" class="subMenuItem">C0RT3X</a> </p></center></div>'
     console.log('01 menu factor loaded')
   }
 
 
 function showMenu() {
-
+    document.getElementById("menuList").innerHTML = '<div class="card-body"><center><p> <a href="./01.html" class="subMenuItem">Home</a> </p><p> <a href="./01_author.html" class="subMenuItem">Author|</a> </p><p> <a href="./01_c0rt3x.html" class="subMenuItem">C0RT3X</a> </p><p> <a href="./01_about.html" class="subMenuItem">About</a> </p></center></div>';
     var x = document.getElementById("menuList");
     if (x.style.display === "none") {
        x.style.display = "block";
@@ -16,3 +15,20 @@ function showMenu() {
     }
  }
 
+ function showA() {
+    document.getElementById("menuList").innerHTML = '<div class="card-body"><center><ul><li><a onclick=" file()">File</a></li><li><a onclick=" edit()">Edit</a></li><li><a onclick=" cortex01()">C0RT3X01</a></li></ul></center></div>'
+
+    var x = document.getElementById("menuList");
+    if (x.style.display === "none") {
+       x.style.display = "block";
+       document.getElementById("abtn").innerHTML = "XX";
+    } else {
+       x.style.display = "none";
+       document.getElementById("abtn").innerHTML = "A|";
+    }
+ }
+function closeA(){
+    document.getElementById("menuList").innerHTML = ''
+    document.getElementById("menuList").style.display = 'none'
+    document.getElementById("abtn").innerHTML = 'A|'
+}
