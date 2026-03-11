b1bd43366e938ebd940193d7f823a5a8e24f7c90
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2026-03-10 19:09:43 -0700

    added delete button

--- source/luminaryide.js
+++ source/luminaryide.js
@@ -91,6 +91,30 @@ startAutoSave();
 
    //End
 
+   // Clears all entries from the store
+async function idbClear() {
+  document.getElementById("textEditor").value =''
+  try {
+    const db = await openDB();
+    return new Promise((resolve, reject) => {
+      const tx      = db.transaction(STORE_NAME, "readwrite");
+      const store   = tx.objectStore(STORE_NAME);
+      const request = store.clear();
+      request.onsuccess = () => {
+        console.log("Store cleared.");
+        resolve();
+      };
+      request.onerror = (event) => reject(event.target.error);
+    });
+    
+  } catch (err) {
+    console.error("idbClear failed:", err);
+  }
+  location.reload(); 
+}
+
+//end
+
 function saveFile() {
     var blob = new Blob([document.getElementById("textEditor").value],
        { type: "text/plain;charset=utf-8" });
