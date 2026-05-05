7c65dd2cb8b3602a627649423c091ab8a23001a7
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2026-05-04 23:01:03 -0700

    updates

--- menu.js
+++ menu.js
@@ -1,7 +1,7 @@
 
 function topMenu_load() {
   document.getElementById("topMenu").innerHTML = '<ul><li><a href="./index.html">B0RG</a></li><li><a class="nbus" onclick="showproducts()">Projects</a></li><li><a class="nbus" onclick="showcontact()">Contact</a></li></ul>'
-  document.getElementById('products').innerHTML = '<div class="card"><div class="card-heading"><h1>Projects.</h1> </div><div class="card-body"><div><h3>C0RT3X 01</h3><p>A human-centric research platform.</p><p><a href="./c0rt3x.html"><button class="abus">Learn More</button></a></p><h3>Project Luminary</h3><p>Revisitng modern code standards.</p><p><a href="./luminary.html"><button class="abus">Learn More</button></a></p><h3>Author</h3><p>A focused writing space.</p><p><a href="./author.html"><button class="abus">Learn More</button></a></p>                <h3>Buy Me a Coffee</h3>  <p>Fighting the AI-slop is not free.</p> <p><a href="https://buymeacoffee.com/jborg_arts" target="_BLANK"><button class="obus" >Buy Me a Coffee</button></a></p></div></div></div>'
+  document.getElementById('products').innerHTML = '<div class="card"><div class="card-heading"><h1>Projects</h1> </div><div class="card-body"><div><h3>C0RT3X 01</h3><p>Find human voices in academia</p><p><a href="./c0rt3x.html"><button class="abus">Learn More</button></a></p><h3>Project Luminary</h3><p>Rediscovering efficient code</p><p><a href="./luminary.html"><button class="abus">Learn More</button></a></p><h3>Author</h3><p>A quiet writing nook</p><p><a href="./author.html"><button class="abus">Learn More</button></a></p>                <h3>Buy Me a Coffee</h3>  <p>Fighting the AI-slop is not free.</p> <p><a href="https://buymeacoffee.com/jborg_arts" target="_BLANK"><button class="obus" >Buy Me a Coffee</button></a></p></div></div></div>'
   console.log('Top menu factor loaded')
 }
 
@@ -17,7 +17,7 @@ function footMenu_load_alt() {
 }
 
 function loadContact() {
-  document.getElementById("contact").innerHTML = '<div class="card"><div class="card-heading"><h1>Contact.</h1></div ><div class="card-body"><center><iframe style="border: none; width: 95%; height: 701px" src="https://docs.getgrist.com/forms/b1EUs21w5g3cSCF67jrWbe/9"></iframe></center></div></div > '
+  document.getElementById("contact").innerHTML = '<div class="card"><div class="card-heading"><h1>Contact</h1></div ><div class="card-body"><center><iframe style="border: none; width: 95%; height: 701px" src="https://docs.getgrist.com/forms/b1EUs21w5g3cSCF67jrWbe/9"></iframe></center></div></div > '
   console.log('Contact factor loaded')
 }
 console.log('All menu factors loaded')
